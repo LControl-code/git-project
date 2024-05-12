81a528f8bab3a5de0f55af0f160f4a9592b7b9e3
Adam Stratilík <adam.stratilik@gstudent.ssnd.sk>
2024-05-12 09:10:50 +0200

    TASK 4b: Pridanie recursive funkcionality

--- main.sh
+++ main.sh
@@ -2,13 +2,26 @@
 
 list_files() {
     local dir="$1"
+    local recurse="${2:-false}"
     for file in "$dir"/*; do
-            echo "$(basename "$file")"
+        if [ -f "$file" ]; then
+            echo "$(realpath "$file")"
+        elif [ "$recurse" = true ] && [ -d "$file" ]; then
+            list_files "$file" "$recurse"
+        fi
     done
 }
 
 main() {
-    list_files $1
+    local dir="$1"
+    local recurse="$2"
+
+    if [ ! -d "$dir" ]; then
+        recurse="$dir"
+        dir="."
+    fi
+
+    list_files "$dir" "$recurse"
 }
 
 main "$@"
